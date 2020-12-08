6d5fd385ca0cc74dea0d589ef5e31b0f8e9fe6ed
Zayra Jeronimo <zayrajero@gmail.com>
2020-12-07 22:31:06 -0600

    ADD Introduction Js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 console.log(Hola)
+
+let name_user = prompt('¿Cuál es tu nombre?')
+
+console.log(name_user)
+
+let age_user = prompt('¿Cuál es tu edad?')
+
+console.log(age_user)
+
+let resultSuma = parseInt(age_user) + 5
+
+console.log('suma: ', resultSuma)
+console.log('multi: ', age_koder * 5)
+console.log('div: ', age_user / 5)
+console.log('rest: ', age_koder - 5)
