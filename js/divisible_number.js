e13c936a20d777350a01232063c84148a00620cf
Zayra Jeronimo <zayrajero@gmail.com>
2020-12-09 15:35:06 -0600

    ADD exercise even odd number

--- js/divisible_number.js
+++ js/divisible_number.js
@@ -1,11 +1,11 @@
 const DIVISIBLE_NUMBER = prompt('Ingresa un número entre 1 y 100')
 
 if (DIVISIBLE_NUMBER % 2 === 0 && DIVISIBLE_NUMBER % 11 === 0) {
-    console.log(DIVISIBLE_NUMBER + ' es divisible entre 2 y 11')
+    console.log(`${DIVISIBLE_NUMBER} es divisible entre 2 y 11`)
 } else if (DIVISIBLE_NUMBER % 2 === 0 && DIVISIBLE_NUMBER % 11 !== 0) {
-    console.log(DIVISIBLE_NUMBER + ' es divisible entre 2 pero no de 11')
+    console.log(`${DIVISIBLE_NUMBER} es divisible entre 2 pero no de 11`)
 } else if (DIVISIBLE_NUMBER % 2 !== 0 && DIVISIBLE_NUMBER % 11 === 0) {
-    console.log(DIVISIBLE_NUMBER + ' no es divisible entre 2 pero si de 11')
+    console.log(`${DIVISIBLE_NUMBER} no es divisible entre 2 pero si de 11`)
 } else {
-    console.log(DIVISIBLE_NUMBER + ' no es un número divisible entre 2 y 11')
+    console.log(`${DIVISIBLE_NUMBER} no es un número divisible entre 2 y 11`)
 }
---
+++ js/even_odd_number.js
@@ -0,0 +1,6 @@
+const EVEN_ODD = prompt('Ingresa un número entre 1 y 100')
+
+EVEN_ODD % 2 === 0 ?
+    console.log(`El ${EVEN_ODD} es número par`)
+    :
+    console.log(`El ${EVEN_ODD} es número impar`)
