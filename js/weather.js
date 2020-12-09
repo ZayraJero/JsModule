5e365439db9f5209a6e38d51691de48c350ae45f
Zayra Jeronimo <zayrajero@gmail.com>
2020-12-08 21:40:40 -0600

    UPDATE weather exercise

--- js/weather.js
+++ js/weather.js
@@ -1,14 +1,32 @@
 const WEATHER = prompt('Ingresa el clima')
 
-if (WEATHER === 'lluvioso') {
-    console.log('esta nublado')
-} else if (WEATHER === 'soleado') {
-    console.log('esta nublado')
-} else if (WEATHER === 'nublado') {
-    console.log('esta soleado')
-} else if (WEATHER === 'nevando') {
-    console.log('esta lluvioso')
-} else {
-    const NEW_WEATHER = prompt('Describe el clima')
-    console.log('asi esta')
-} 
+switch (WEATHER) {
+    case 'lluvioso':
+        console.log('esta nublado')
+        break
+    case 'soleado':
+        console.log('esta nublado')
+        break
+    case 'nublado':
+        console.log('esta soleado')
+        break
+    case 'nevando':
+        console.log('esta lluvioso')
+        break
+    default:
+        const NEW_WEATHER = prompt('Describe el clima')
+        console.log('asi esta')
+}
+
+// if (WEATHER === 'lluvioso') {
+//     console.log('esta nublado')
+// } else if (WEATHER === 'soleado') {
+//     console.log('esta nublado')
+// } else if (WEATHER === 'nublado') {
+//     console.log('esta soleado')
+// } else if (WEATHER === 'nevando') {
+//     console.log('esta lluvioso')
+// } else {
+//     const NEW_WEATHER = prompt('Describe el clima')
+//     console.log('asi esta')
+// } 
