4a698f5e32ea2be269569528553b9943d9ed5dd9
Zayra Jeronimo <zayrajero@gmail.com>
2020-12-10 16:15:15 -0600

    ADD exercise one

--- js/strmth_for_exercises.js
+++ js/strmth_for_exercises.js
@@ -2,24 +2,17 @@
 // 7 x 1 = 7
 for (let i = 1; i <= 10; i++) {
     multi = 7 * i
-    // console.log(multi)
+    console.log(`7 x ${i} = ${multi}`)
 }
 // imprimir en consola los múltiplos de 3 y 7  que se encuentran entre el 1 y el 100
 // 3,6,7,9,12,14,15....
 for (let i = 1; i <= 100; i++) {
     mltp = 3 * i
     multipli = 7 * i
-    console.log(`${mltp} ${multipli}`)
+    // console.log(`${mltp} ${multipli}`)
 }
-
 // imprimir en consola la cantidad de espacios que se encuentran en una cadena de texto
 // “en un lugar de la mancha” -> 5
 
 // imprimir en consola la cantidad de vocales que se encuentran en una cadena de texto
 // “Hola mundo” -> 4
-
-3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42,
-    45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81,
-    84, 87, 90, 91, 93, 96, 98, 99
-
-7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98
