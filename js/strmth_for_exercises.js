a2c9e59ef3aee9558caa230e79e054f5d6b1683d
Zayra Jeronimo <zayrajero@gmail.com>
2020-12-10 14:07:35 -0600

    ADD exercise one

---
+++ js/strmth_for_exercises.js
@@ -0,0 +1,20 @@
+// Imprimir en consola la tabla del 7
+// 7 x 1 = 7
+for (let i = 1; i <= 10; i++) {
+    multi = 7 * i
+    console.log(multi)
+}
+// imprimir en consola los múltiplos de 3 y 7  que se encuentran entre el 1 y el 100
+// 3,6,7,9,12,14,15....
+
+// imprimir en consola la cantidad de espacios que se encuentran en una cadena de texto
+// “en un lugar de la mancha” -> 5
+
+// imprimir en consola la cantidad de vocales que se encuentran en una cadena de texto
+// “Hola mundo” -> 4
+
+3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42,
+    45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81,
+    84, 87, 90, 93, 96, 99
+
+7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98
