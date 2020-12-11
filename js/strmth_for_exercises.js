bb7fb2bdf009cdd14693d2a0d570e196c735899c
Zayra Jeronimo <zayrajero@gmail.com>
2020-12-10 19:41:40 -0600

    ADD exercises

--- js/strmth_for_exercises.js
+++ js/strmth_for_exercises.js
@@ -1,13 +1,12 @@
 // Imprimir en consola la tabla del 7
 // 7 x 1 = 7
 for (let i = 1; i <= 10; i++) {
-    multi = 7 * i
-    // console.log(`7 x ${i} = ${multi}`)
+    // console.log(`7 x ${i} = ${7 * i}`)
 }
 // imprimir en consola los múltiplos de 3 y 7  que se encuentran entre el 1 y el 100
 // 3,6,7,9,12,14,15....
-for (let i = 1; i < 100; i++) {
-    if (i % 3 === 0 && i % 7 === 0) {
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 || i % 7 === 0) {
         // console.log(i)
     }
 }
@@ -18,10 +17,10 @@ let count = 0
 
 for (i = 0; i < text.length; i++) {
     if (text[i] === ' ') {
-        count++
+        count += 1
     }
 }
-// console.log(`la frase tiene ${count} espacios`)
+console.log(`la frase tiene ${count} espacios`)
 // imprimir en consola la cantidad de vocales que se encuentran en una cadena de texto
 // “Hola mundo” -> 4
 const txt = 'Hola mundo'
