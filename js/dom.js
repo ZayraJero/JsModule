543317e45224f7d60657f9d433d7330277e55466
Zayra Jeronimo <zayrajero@gmail.com>
2020-12-22 18:41:14 -0600

    ADD class

---
+++ js/dom.js
@@ -0,0 +1,33 @@
+// create nodes and append
+let title = document.createElement("h1")
+let texto = document.createTextNode('Soy un titulo desde JS')
+title.appendChild(texto)
+document.getElementById('caja2').appendChild(title)
+
+let parrafo = document.createElement('p')
+let textoparrafo = document.createTextNode('soy un parrafo desde JS')
+parrafo.appendChild(textoparrafo)
+
+console.log(parrafo)
+document.getElementById('caja2').appendChild(parrafo)
+
+
+// selectores JS
+
+
+// let elemento = document.getElementById('elementToSet')
+let elementos = document.getElementsByClassName('parrafos')
+let secondElement = elementos[1]
+// secondElement.setAttribute('class','parrafos parrafo2 parrafoprueba active')
+secondElement.classList.add('active')
+secondElement.classList.remove('parrafoprueba')
+let bytag = document.getElementsByTagName('section')
+console.log(bytag)
+bytag[0].classList.add('section__parrafo')
+
+let secciones = document.getElementsByClassName('secciones')
+// let secciones = document.getElementsByClassName('sinclase')
+// let querySelector = document.querySelector('.secciones')
+// 1er coincidencia
+let querySelectorAll = document.querySelectorAll('li')
+console.log(querySelectorAll)
