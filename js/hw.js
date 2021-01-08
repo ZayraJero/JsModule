9006ec0c2254f732716987189c697bd13966247d
Zayra Jeronimo <zayrajero@gmail.com>
2021-01-08 16:07:25 -0600

    ADDED class

---
+++ js/hw.js
@@ -0,0 +1,18 @@
+// HomeWork
+// 1.
+// si el usuario no tiene la cookie sessionuser
+// Redireccionarlo al /login.html
+// si tiene la cookie sessionuser
+// redireccionarlo a /user.html
+
+// 2. 
+// Crear un div con un mensaje de
+// "Este sitio se ve mejor en desktop"
+
+// si el viewportWidth es <= 767
+// aparecer el mensaje
+// sino
+// desaparecer el mensaje
+
+// utilizar css
+// no usar inline styles
