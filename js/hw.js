78372cf7e9271f60d89d68cdac58f2e4ac1814fc
Zayra Jeronimo <zayrajero@gmail.com>
2021-01-15 17:35:56 -0600

    ADDED hw jquery

--- js/hw.js
+++ js/hw.js
@@ -3,3 +3,12 @@
  * Cuando el usuario ponga cualquier ciudad en el input de busqueda
  * de la lista de ciudades, filtrar solo las que coincidan con la busqueda
  */
+
+$(document).ready(function () {
+    $("#busqueda").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#ciudades li").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
