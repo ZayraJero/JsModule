a9ccb3385ebe3b6c56dc04a00c42c6c0737d5154
Zayra Jeronimo <zayrajero@gmail.com>
2021-01-15 16:14:41 -0600

    ADDED class

---
+++ js/hw.js
@@ -0,0 +1,5 @@
+// dado el layout homework.html
+/**
+ * Cuando el usuario ponga cualquier ciudad en el input de busqueda
+ * de la lista de ciudades, filtrar solo las que coincidan con la busqueda
+ */
