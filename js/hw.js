6540386cff292bea5950333ad2653ecd795c6346
Zayra Jeronimo <zayrajero@gmail.com>
2021-01-18 17:53:50 -0600

    ADD class ajax

--- js/hw.js
+++ js/hw.js
@@ -4,11 +4,29 @@
  * de la lista de ciudades, filtrar solo las que coincidan con la busqueda
  */
 
-$(document).ready(function () {
-    $("#busqueda").on("keyup", function () {
-        var value = $(this).val().toLowerCase();
-        $("#ciudades li").filter(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
+$("#busqueda").keyup(function () {
+    let value = $(this).val().toLowerCase();
+    $("#ciudades li").filter(function () {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
 });
+
+//otra solucion
+// $('#busqueda').keyup(function(){
+
+//     let term = $(this).val()
+//     $('#ciudades li').hide()
+//     let ciudades = $("#ciudades li")
+//     for(item of ciudades) {
+//         console.log(item)
+//         if (item.innerText.toLowerCase().search(term.toLowerCase()) !== -1) { 
+//             $(item).show()
+//         }
+//     }
+//     $("#ciudades li").each((id, elem) => { 
+//         if (elem.innerText.toLowerCase().search(term.toLowerCase()) !== -1) { 
+//             $("#ciudades li").eq(id).show()
+//         } 
+//     })
+
+// })
---
+++ js/traversing.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    //$('b').parent().prev().find('span')
+    //$('b').parent().parent().find('li:first')
+
+
+    // .parent()
+    // .parent('selector')
+    // .prev()
+    // .prev('selector')
+    // .closest()
+    // .closest('selector')
+
+
+    // .children()
+    // .find()
+    // .next()
+    // .next('arg') 
+
+})
